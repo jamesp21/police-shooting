7596c91c7521f2727066fb90f594197eb2256891
James-Porter <porterjames1993@yahoo.com>
2015-10-19 00:32:21 -0700

    i added the map and title but could not figure out how to get the data on to the map, but plan to try in the morning

--- Readme.md
+++ Readme.md
@@ -1,3 +1,18 @@
 # Police shootings
 
 This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/map-challenge).
+##### 1. What is the URL of this site, as hosted on your student webserver (i.e., students.washington.edu/<code>{uw-new-id}</code>/info343/<code>{project-name}</code>) #####
+> Answer goes here!
+students.washington.edu/<code>jamesp21</code>/info343/<code>police-shooting</code>
+##### 2. Did you receive any help from others (classmates, etc)? If so, please list who. #####
+> Answer goes here!
+no
+##### 3. Did you complete any advanced extensions to this challenge? If so, what? #####
+> Answer goes here!
+no
+##### 4. Approximately how many hours did it take you to complete this challenge? #####
+> Answer goes here!
+i haven't finished. but i spent 8 hours on it
+##### 5. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better? #####
+> Answer goes here!
+With windows/pc/ gitbash there was no way i could figure how to do the python command. Also, the for loop section i dont know how to grab the data from the getData function. I might sound dumb as the answer was simply call getData but idk.
--- js/map.js
+++ js/map.js
@@ -1,35 +1,68 @@
 // Function to draw your map
 var drawMap = function() {
+  /* example fromleafletjs.com
+  var map = L.map('map').setView([51.505, -0.09], 13);
 
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+    */
   // Create map and set view
- 
+  var map = L.map('container').setView([38.75, -121.46], 4);
 
   // Create a tile layer variable using the appropriate url
-
+  var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
 
   // Add the layer to your map
- 
+  layer.addTo(map);
+  L.marker([38.75, -121.46]).addTo(map)
+    .bindPopup('Officers responded to a report of a vehicle prowler. Subject was shot and fataly wounded after officers arrived on the scene. Report states that subject had a gun in his possession. (link)')
+    .openPopup();
 
   // Execute your function to get data
- 
-}
+ getData();
+};
 
 // Function for getting data
 var getData = function() {
 
   // Execute an AJAX request to get the data in data/response.js
-
+  $.ajax({
+    url:'data/response.json',
+    type: "get",
+    success:function(dat) {
+      data = dat
+    }, 
+   dataType:"json"
+}); 
 
   // When your request is successful, call your customBuild function
-
-}
+  customBuild();
+};
 
 // Loop through your data and add the appropriate layers and points
 var customBuild = function() {
 	// Be sure to add each layer to the map
+	var index;
+	var layerName = new L.LayerGroup([circles]);
+	for(index = 0; index < getData().length; ++index){
+		var info = getData[index];
+		var circle = new L.circleMarker([info.lat, info.lng]);
+        circle.addTo(cities);
+        circle.bindPopup(info.summary);
+	}
+	/*var layerName = new L.LayerGroup([]));
+    var circle = new L.circleMarker([latitude, longitude], options));
+    circle.addTo(layer));
+    circle.bindPopup(text));
 
 	// Once layers are on the map, add a leaflet controller that shows/hides layers
-  
-}
+    L.control.layers(null,layers).addTo(map);*/
+    L.control.cities(null,cities).addTo(map);
+};
 
 
