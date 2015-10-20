20dec0dd9d49e3b31c2b6474ff65bc68e88ff1d9
James-Porter <porterjames1993@yahoo.com>
2015-10-20 04:06:23 -0700

    I finally finished and feel better

--- Readme.md
+++ Readme.md
@@ -6,13 +6,13 @@ This repository is a challenge for the following [course](http://faculty.washing
 students.washington.edu/<code>jamesp21</code>/info343/<code>police-shooting</code>
 ##### 2. Did you receive any help from others (classmates, etc)? If so, please list who. #####
 > Answer goes here!
-no
+Jullian, Mike
 ##### 3. Did you complete any advanced extensions to this challenge? If so, what? #####
 > Answer goes here!
-no
+maybe, i forgot
 ##### 4. Approximately how many hours did it take you to complete this challenge? #####
 > Answer goes here!
-i haven't finished. but i spent 8 hours on it
+20 hours
 ##### 5. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better? #####
 > Answer goes here!
-With windows/pc/ gitbash there was no way i could figure how to do the python command. Also, the for loop section i dont know how to grab the data from the getData function. I might sound dumb as the answer was simply call getData but idk.
+With windows/pc/ and python 3.5 there is a different way of using the local server. was confusing on putting the info box about the races on top of map.
--- js/map.js
+++ js/map.js
@@ -1,28 +1,23 @@
+var data;
+var map;
+var whiteMen = 0;
+var otherWhites = 0;
+var nonWhiteMen = 0;
+var otherNonWhites = 0;
+
 // Function to draw your map
 var drawMap = function() {
-  /* example fromleafletjs.com
-  var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-    */
   // Create map and set view
-  var map = L.map('container').setView([38.75, -121.46], 4);
+   map = L.map('container').setView([38.69, -81.56], 4);
 
   // Create a tile layer variable using the appropriate url
   var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
 
   // Add the layer to your map
   layer.addTo(map);
-  L.marker([38.75, -121.46]).addTo(map)
-    .bindPopup('Officers responded to a report of a vehicle prowler. Subject was shot and fataly wounded after officers arrived on the scene. Report states that subject had a gun in his possession. (link)')
+  L.marker([45.75, -60.46]).addTo(map)
+    .bindPopup('This map contains the following race: Unknown, White, Black or African American, Asian American, Indian or Alaska Native, Native Hawaiian or Other Pacific islander')
     .openPopup();
-
   // Execute your function to get data
  getData();
 };
@@ -35,34 +30,47 @@ var getData = function() {
     url:'data/response.json',
     type: "get",
     success:function(dat) {
-      data = dat
+      data = dat;
+      customBuild();
     }, 
    dataType:"json"
 }); 
 
   // When your request is successful, call your customBuild function
-  customBuild();
 };
 
 // Loop through your data and add the appropriate layers and points
 var customBuild = function() {
 	// Be sure to add each layer to the map
-	var index;
-	var layerName = new L.LayerGroup([circles]);
-	for(index = 0; index < getData().length; ++index){
-		var info = getData[index];
-		var circle = new L.circleMarker([info.lat, info.lng]);
-        circle.addTo(cities);
-        circle.bindPopup(info.summary);
+	var totalMen = 0;
+	var totalPeople = data.length;
+	var layerName = new L.LayerGroup([]);
+	for(i = 0; i < totalPeople; i++){
+		var info = data[i];
+		if (info["Hit or Killed?"] == 'Killed') {
+		var circle = new L.circleMarker([info.lat, info.lng], {color: "red"});
+		}else {
+		var circle = new L.circleMarker([info.lat, info.lng], {color: "black"});
+		}
+        circle.addTo(layerName);
+        circle.bindPopup(info.Summary + " " + info["Source Link"]);
+        if (info["Victim's Gender"] == "Male") {
+        	totalMen++;
+        	if (info["Race"] == "White") {
+        		whiteMen++;
+        	}
+        }else {
+        	if (info["Race"] == "White") {
+        		otherWhites++;
+        	}
+        }
 	}
-	/*var layerName = new L.LayerGroup([]));
-    var circle = new L.circleMarker([latitude, longitude], options));
-    circle.addTo(layer));
-    circle.bindPopup(text));
-
+	layerName.addTo(map);
+	nonWhiteMen = totalMen - whiteMen;
+	otherNonWhites = totalPeople - (totalMen + otherWhites);
 	// Once layers are on the map, add a leaflet controller that shows/hides layers
-    L.control.layers(null,layers).addTo(map);*/
-    L.control.cities(null,cities).addTo(map);
+	document.getElementById("p1").innerHTML = whiteMen;
+	document.getElementById("p2").innerHTML = otherWhites;
+	document.getElementById("p3").innerHTML = nonWhiteMen;
+	document.getElementById("p4").innerHTML = otherNonWhites;
 };
-
-
